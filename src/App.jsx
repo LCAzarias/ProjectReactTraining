ce8a5bf015e4b9edd5448d18b2a1bf8de42e396e
Luis <akade.luis@gmail.com>
2026-06-11 07:42:41 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
+import { useState } from "react";
 import Tasks from "./components/Tasks";
 import AddTasks from "./components/AddTasks";
 function App() {
-const [tasks, setTasks] = useState([
-  id: 1,
-  title: "Estudar Programação",
-  description: "Estudar React, JavaScript e outras tecnologias de desenvolvimento web.",
-  isCompleted: false,
-]);
-
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "ESTUDAR INGLÊS",
+      description: "Melhorar o inglês",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "ESTUDAR PYTHON",
+      description: "Desenvolvimento Web",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "ESTUDAR JAVASCRIPT",
+      description: "Formação Full Stack",
+      isCompleted: false,
+    },
+  ]);
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-125">
-        <h1 className="text-3xl text-slate-50 font-bold text-center">GERENCIADOR DE TAREFAS</h1>
-        <Tasks />
+        <h1 className="text-3xl text-slate-50 font-bold text-center">
+          GERENCIADOR DE TAREFAS
+        </h1>
         <AddTasks />
+        <Tasks tasks={tasks} />
       </div>
     </div>
   );
 }
 
 export default App;
-
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,13 @@
-function Tasks() {
+function Tasks(props) {
   return (
-    <div>
-      <h1>Tasks</h1>
-    </div>
+    <ul className="space-y-4 p-6 bg-slate-300 rounded-md shadow-md">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="bg-slate-400 text-white p-2 rounded-md">
+          {task.title}
+          <button>Ver Detalhes</button>
+        </li>
+      ))}
+    </ul>
   );
 }
-
 export default Tasks;

9a41e39bf8ef504240b201a7ee8b3cbe23603340
Luis <akade.luis@gmail.com>
2026-06-17 06:30:49 -0300

    Update

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,8 +2,8 @@ function Tasks(props) {
   return (
     <ul className="space-y-4 p-6 bg-slate-300 rounded-md shadow-md">
       {props.tasks.map((task) => (
-        <li key={task.id} className="bg-slate-400 text-white p-2 rounded-md">
-          {task.title}
+        <li key={task.id} className="flex items-center justify-between">
+          <button className="bg-slate-400 w-full text-white p-2 rounded-md">{task.title}</button>
           <button>Ver Detalhes</button>
         </li>
       ))}

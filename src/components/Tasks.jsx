b49c9bea174585bbab8c729b0b661ae61c169e8f
Luis <akade.luis@gmail.com>
2026-06-17 07:03:02 -0300

    Update II

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,12 @@
+import { ChevronRightIcon } from "lucide-react";
 function Tasks(props) {
   return (
     <ul className="space-y-4 p-6 bg-slate-300 rounded-md shadow-md">
       {props.tasks.map((task) => (
-        <li key={task.id} className="flex items-center justify-between">
-          <button className="bg-slate-400 w-full text-white p-2 rounded-md">{task.title}</button>
-          <button>Ver Detalhes</button>
+        <li key={task.id} className="flex gap-2">
+          <button className="bg-slate-400 w-full text-left text-white p-2 rounded-md">{task.title}</button>
+          <button className="bg-slate-400 p-2 rounded-md text-white"><ChevronRightIcon/></button>
+
         </li>
       ))}
     </ul>
